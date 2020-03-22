afc4395056804d4374074e21542ee2aafe1ccf31
Dante Calderon <mail@dantecalderon.dev>
2020-03-22 00:25:45 -0400

    Add settings

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const gTTS = require('gtts');
 var gtts = new gTTS('Dante eres un pendejo', 'es');
 
 gtts.save('/tmp/hello.mp3', function (err, result) {
-  if(err) { throw new Error(err) }
+  if (err) { throw new Error(err) }
   console.log('Success! Open file /tmp/hello.mp3 to hear result.');
 });
